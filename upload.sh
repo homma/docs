9d90e514c080d5c7ccabe8c2b5fe39fe59586bf3
homma <daisuke.homma@gmail.com>
2018-12-02 20:20:43 +0900

    committed on 2018/12/02 20:20.

--- upload.sh
+++ upload.sh
@@ -26,12 +26,13 @@
 
 DATE=`date '+%Y/%m/%d %H:%M'`
 MESSAGE="committed on ${DATE}."
+SCRIPT_DIR=$(dirname $0)
 
 if [ $# -eq 1 ]; then MESSAGE=$1; fi
 
 git pull
 git checkout master
-git add .
+git add ${SCRIPT_DIR}
 git commit -a -m "${MESSAGE}"
 git push origin master
 
